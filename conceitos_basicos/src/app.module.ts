0eda64238bd636a2c1c160a60136ccfd61e2e5f6
Guilherme <guilhermemendesdev@gmail.com>
2025-01-04 07:35:11 -0300

    feat: referencia do modulo de recados no AppModule

--- conceitos_basicos/src/app.module.ts
+++ conceitos_basicos/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConceitosManualModule } from './conceitos-manual/conceitos-manual.module';
 import { ConceitosAutomaticoModule } from './conceitos-automatico/conceitos-automatico.module';
+import { RecadosModule } from './recados/recados.module';
 
 @Module({
-  imports: [ConceitosManualModule, ConceitosAutomaticoModule],
+  imports: [ConceitosManualModule, ConceitosAutomaticoModule, RecadosModule],
   controllers: [AppController],
   providers: [AppService],
 })
