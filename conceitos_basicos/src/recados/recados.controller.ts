2c1b8bcbb8768db557577d5076a81bfa5e0ce58d
Guilherme <guilhermemendesdev@gmail.com>
2025-01-04 07:35:25 -0300

    feat: criação do controller de recados

---
+++ conceitos_basicos/src/recados/recados.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('recados')
+export class RecadosController {
+  @Get()
+  findAll() {
+    return 'Todos os recados';
+  }
+
+  @Get('/:id')
+  findOne(@Param('id') id: string) {
+    return 'Apenas UM recado';
+  }
+}
