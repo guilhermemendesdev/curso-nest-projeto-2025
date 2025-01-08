6839997df9aa12af22aba20f15f03219de17b8f5
Guilherme <guilhermemendesdev@gmail.com>
2025-01-08 06:06:42 -0300

    feat: adiciona validação de segurança, evitando que envie campos não listados no DTO

--- conceitos_basicos/src/main.ts
+++ conceitos_basicos/src/main.ts
@@ -4,7 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Retira do corpo da requisição campos não listados no DTO
+      forbidNonWhitelisted: true, //Dispara erro ao enviar campos não listados no DTO
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
