5303b370207cac278d4205d0a14f4b4ad1738f3e
Guilherme <guilhermemendesdev@gmail.com>
2025-01-09 06:42:04 -0300

    feat: aponta entity de Recados em recadosModule

--- conceitos_basicos/src/recados/recados.module.ts
+++ conceitos_basicos/src/recados/recados.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { RecadosController } from './recados.controller';
 import { RecadosService } from './recados.service';
+import { Recado } from './entities/recado.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Recado])],
   controllers: [RecadosController],
   providers: [RecadosService],
 })
