686d13521b911a503a06506a9c96340eae06ba53
Guilherme <guilhermemendesdev@gmail.com>
2025-01-09 06:30:27 -0300

    feat: configura o TypeOrmModule com PostgreSQL

--- conceitos_basicos/src/app.module.ts
+++ conceitos_basicos/src/app.module.ts
@@ -4,9 +4,23 @@ import { AppService } from './app.service';
 import { ConceitosManualModule } from './conceitos-manual/conceitos-manual.module';
 import { ConceitosAutomaticoModule } from './conceitos-automatico/conceitos-automatico.module';
 import { RecadosModule } from './recados/recados.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConceitosManualModule, ConceitosAutomaticoModule, RecadosModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true, // Carrega entidades sem precisar especificá-las
+      synchronize: true, // Sincroniza com o DB. Não deve ser usado em produção
+    }),
+    ConceitosManualModule,
+    ConceitosAutomaticoModule,
+    RecadosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
