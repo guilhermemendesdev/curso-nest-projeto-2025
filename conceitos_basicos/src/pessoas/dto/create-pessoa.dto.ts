516086b65aaea496aff2e3e8c0180edf6c6454a2
Guilherme <guilhermemendesdev@gmail.com>
2025-01-10 06:07:14 -0300

    feat: cria DTO de create-pessoa

---
+++ conceitos_basicos/src/pessoas/dto/create-pessoa.dto.ts
@@ -0,0 +1,23 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePessoaDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(50)
+  nome: string;
+}
