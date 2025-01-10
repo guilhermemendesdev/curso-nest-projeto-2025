54655de4e7b2fa5a4b75447d685f349d409680d1
Guilherme <guilhermemendesdev@gmail.com>
2025-01-10 06:08:27 -0300

    feat: finaliza função para criar nova pessoa

---
+++ conceitos_basicos/src/pessoas/pessoas.service.ts
@@ -0,0 +1,49 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreatePessoaDto } from './dto/create-pessoa.dto';
+import { UpdatePessoaDto } from './dto/update-pessoa.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Pessoa } from './entities/pessoa.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class PessoasService {
+  constructor(
+    @InjectRepository(Pessoa)
+    private readonly pessoaRepository: Repository<Pessoa>,
+  ) {}
+
+  async create(createPessoaDto: CreatePessoaDto) {
+    try {
+      const dadosPessoa = {
+        email: createPessoaDto.email,
+        nome: createPessoaDto.nome,
+        passwordHash: createPessoaDto.password,
+      };
+
+      const novaPessoa = this.pessoaRepository.create(dadosPessoa);
+      await this.pessoaRepository.save(novaPessoa);
+      return novaPessoa;
+    } catch (err) {
+      if (err.code === '23505')
+        throw new ConflictException('E-mail já está cadastrado.');
+
+      throw err;
+    }
+  }
+
+  findAll() {
+    return `This action returns all pessoas`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} pessoa`;
+  }
+
+  update(id: number, updatePessoaDto: UpdatePessoaDto) {
+    return `This action updates a #${id} pessoa`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pessoa`;
+  }
+}
