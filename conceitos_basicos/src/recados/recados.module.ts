7ded7e5acc5ca584b66d2f6a38b3b643eb6549d6
Guilherme <guilhermemendesdev@gmail.com>
2025-01-11 06:17:17 -0300

    feat: referenciando PessoasService em RecadoModule

--- conceitos_basicos/src/recados/recados.module.ts
+++ conceitos_basicos/src/recados/recados.module.ts
@@ -3,9 +3,10 @@ import { RecadosController } from './recados.controller';
 import { RecadosService } from './recados.service';
 import { Recado } from './entities/recado.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PessoasModule } from 'src/pessoas/pessoas.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Recado])],
+  imports: [TypeOrmModule.forFeature([Recado]), PessoasModule],
   controllers: [RecadosController],
   providers: [RecadosService],
 })
