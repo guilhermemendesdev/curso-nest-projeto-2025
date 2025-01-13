986e367ea9144c9f9338bcbdcc1f40a44d7e79c8
Guilherme <guilhermemendesdev@gmail.com>
2025-01-13 07:19:15 -0300

    feat: injeta de forma global o ParseIntIdPipe

--- conceitos_basicos/src/main.ts
+++ conceitos_basicos/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ParseIntIdPipe } from './common/pipes/parse-int-id.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
       whitelist: true, //Retira do corpo da requisição campos não listados no DTO
       forbidNonWhitelisted: true, //Dispara erro ao enviar campos não listados no DTO
     }),
+    new ParseIntIdPipe(),
   );
   await app.listen(3000);
 }
