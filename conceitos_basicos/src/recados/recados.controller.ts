8ae4eab7978b16015bf85238b070e9c0b9381075
Guilherme <guilhermemendesdev@gmail.com>
2025-01-14 06:22:14 -0300

    feat: adiciona AddHeaderInterceptor na rota de findAll do RecadosController, adicionando um Header customizado

--- conceitos_basicos/src/recados/recados.controller.ts
+++ conceitos_basicos/src/recados/recados.controller.ts
@@ -7,17 +7,20 @@ import {
   Patch,
   Post,
   Query,
+  UseInterceptors,
 } from '@nestjs/common';
 import { RecadosService } from './recados.service';
 import { CreateRecadoDto } from './dto/create-recado.dto';
 import { UpdateRecadoDto } from './dto/update-recado.dto';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 
 @Controller('recados')
 export class RecadosController {
   constructor(private readonly recadosService: RecadosService) {}
 
   @Get()
+  @UseInterceptors(AddHeaderInterceptor)
   findAll(@Query() paginationDto: PaginationDto) {
     return this.recadosService.findAll(paginationDto);
   }
