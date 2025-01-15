a807204b584b5b3e9013f09500f200a3927eddc6
Guilherme <guilhermemendesdev@gmail.com>
2025-01-15 07:25:40 -0300

    feat: utiliza AuthTokenInterceptor em todo o controller de Recados

--- conceitos_basicos/src/recados/recados.controller.ts
+++ conceitos_basicos/src/recados/recados.controller.ts
@@ -16,9 +16,10 @@ import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { AddHeaderInterceptor } from 'src/common/interceptors/add-header.interceptor';
 import { TimingConnectionInterceptor } from 'src/common/interceptors/timing-connection.interceptor';
 import { ErrorHandlingInterceptor } from 'src/common/interceptors/error-handling.interceptor';
+import { AuthTokenInterceptor } from 'src/common/interceptors/auth-token.interceptor';
 
 @Controller('recados')
-@UseInterceptors(ErrorHandlingInterceptor)
+@UseInterceptors(AuthTokenInterceptor, ErrorHandlingInterceptor)
 export class RecadosController {
   constructor(private readonly recadosService: RecadosService) {}
 
