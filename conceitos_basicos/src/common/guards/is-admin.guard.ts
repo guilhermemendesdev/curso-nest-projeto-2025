a59ed8f269f9261345f9754e70c378869004e568
Guilherme <guilhermemendesdev@gmail.com>
2025-01-16 07:18:59 -0300

    feat: cria IsAdminGuard para verificar as permissões do usuario logado

---
+++ conceitos_basicos/src/common/guards/is-admin.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivate, ExecutionContext } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+export class IsAdminGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const role = request['user']?.role;
+    return role === 'admin';
+  }
+}
