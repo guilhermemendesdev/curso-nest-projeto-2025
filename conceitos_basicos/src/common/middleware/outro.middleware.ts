ba2d49c1f742eba9cc1dd72202260966fcdf35ee
Guilherme <guilhermemendesdev@gmail.com>
2025-01-16 06:36:04 -0300

    feat: cria outra middleware basica para ser executada antes do nestjs

---
+++ conceitos_basicos/src/common/middleware/outro.middleware.ts
@@ -0,0 +1,23 @@
+import { NestMiddleware } from '@nestjs/common';
+
+export class OutroMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: (error?: Error | any) => void) {
+    const authorization = req.headers?.authorization;
+
+    if (authorization) {
+      req['user'] = {
+        nome: 'Guilherme',
+        sobrenome: 'Mendes',
+      };
+    }
+
+    res.setHeader('CABECALHO', 'Do Middleware');
+
+    // Terminando a cadeia de chamadas
+    // return res.status(404).send({
+    //   message: 'Não encontrado',
+    // });
+    next();
+    console.log('OutroMiddleware: Tchau');
+  }
+}
