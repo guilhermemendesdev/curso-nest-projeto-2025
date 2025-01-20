e57619889db7c97013924daf094d946199f84871
Guilherme <guilhermemendesdev@gmail.com>
2025-01-19 22:28:53 -0300

    feat: importa modulo de recados em PessoasModule

--- conceitos_basicos/src/pessoas/pessoas.module.ts
+++ conceitos_basicos/src/pessoas/pessoas.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { PessoasController } from './pessoas.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pessoa } from './entities/pessoa.entity';
+import { RecadosModule } from 'src/recados/recados.module';
 
 @Module({
   imports: [
