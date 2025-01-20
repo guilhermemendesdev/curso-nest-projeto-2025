79fddfaee7f7698abc3961594d0c149f64272506
Guilherme <guilhermemendesdev@gmail.com>
2025-01-20 06:53:30 -0300

    feat: prover funções de remover espacos e remover letras minusculas em RecadosModule

--- conceitos_basicos/src/recados/recados.module.ts
+++ conceitos_basicos/src/recados/recados.module.ts
@@ -5,7 +5,13 @@ import { Recado } from './entities/recado.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
 import { RecadosUtils, RecadosUtilsMock } from './recados.utils';
-import { SERVER_NAME } from 'src/common/constants/server-name.constant';
+import {
+  ONLY_LOWERCASE_LETTERS_REGEX,
+  REMOVE_SPACES_REGEX,
+  SERVER_NAME,
+} from './recados.constants';
+import { OnlyLowerCaseLettersRegex } from 'src/common/regex/only-lowercase-letters.regex';
+import { RemoveSpacesRegex } from 'src/common/regex/remove-spaces.regex';
 
 @Module({
   imports: [
@@ -23,6 +29,14 @@ import { SERVER_NAME } from 'src/common/constants/server-name.constant';
     {
       provide: SERVER_NAME,
       useValue: 'My Name Is NestJS',
+    },
+    {
+      provide: ONLY_LOWERCASE_LETTERS_REGEX,
+      useClass: OnlyLowerCaseLettersRegex,
+    },
+    {
+      provide: REMOVE_SPACES_REGEX,
+      useClass: RemoveSpacesRegex,
     },
   ],
   exports: [
