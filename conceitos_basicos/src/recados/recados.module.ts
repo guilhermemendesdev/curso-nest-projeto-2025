6f2f3bab46c051650557bfe228c3580c81717f1f
Guilherme <guilhermemendesdev@gmail.com>
2025-01-21 07:29:57 -0300

    feat: prover regexFactory no RecadosController

--- conceitos_basicos/src/recados/recados.module.ts
+++ conceitos_basicos/src/recados/recados.module.ts
@@ -5,6 +5,11 @@ import { Recado } from './entities/recado.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from 'src/pessoas/pessoas.module';
 import { RecadosUtils } from './recados.utils';
+import { RegexFactory } from 'src/common/regex/regex.factory';
+import {
+  ONLY_LOWERCASE_LETTERS_REGEX,
+  REMOVE_SPACES_REGEX,
+} from './recados.constants';
 
 @Module({
   imports: [
@@ -12,7 +17,27 @@ import { RecadosUtils } from './recados.utils';
     forwardRef(() => PessoasModule),
   ],
   controllers: [RecadosController],
-  providers: [RecadosService],
+  providers: [
+    RecadosService,
+    RecadosUtils,
+    RegexFactory,
+    {
+      provide: REMOVE_SPACES_REGEX,
+      useFactory: (RegexFactory: RegexFactory) => {
+        // Meu código/Lógica
+        return RegexFactory.create('RemoveSpacesRegex');
+      }, // Factory
+      inject: [RegexFactory], //Injetando na factory na ordem
+    },
+    {
+      provide: ONLY_LOWERCASE_LETTERS_REGEX,
+      useFactory: (RegexFactory: RegexFactory) => {
+        // Meu código/Lógica
+        return RegexFactory.create('OnlyLowercaseLettersRegex');
+      }, // Factory
+      inject: [RegexFactory], //Injetando na factory na ordem
+    },
+  ],
   exports: [RecadosUtils],
 })
 export class RecadosModule {}
